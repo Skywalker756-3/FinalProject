2f646445c25a4901ed73b373dc9f706a7d750d89
Mahnoor Imtiaz <mahnoorimtiaz@trentu.ca>
2025-03-13 02:04:06 -0400

    Update images for all landing pages media

---
+++ script.js
@@ -0,0 +1,7 @@
+//Landing Page
+// Define the navigateTo function
+function navigateTo(url) {
+    window.location.href = url; // Redirects to the specified URL
+  }
+//Section 1 - Interactive horoscope
+
